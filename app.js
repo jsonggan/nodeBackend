93024ff53f6ac95773f5df9466408c0d39a8e8ef
chin song <1006283@mymail.sutd.edu.sg>
2023-03-15 15:25:41 +0800

    sync

--- app.js
+++ app.js
@@ -4,27 +4,42 @@ const fs = require('fs');
 const server = http.createServer((req,res)=>{
     const url = req.url;
     const method = req.method;
-    if (url =='/'){
+    
+    if (url ==='/'){
         res.write('<html>');
         res.write('<head><title>My First Backend</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type= "submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
-    if (url ==='/message' && method ==='POST'){
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
+    if (url === '/message' && method === 'POST') {
+        console.log('go through message');
+        const body = [];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+            console.log(parsedBody);
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+        
+
     }
-    console.log(req.url, req.method,req.headers);
+    
+    // console.log(req.url, req.method,req.headers);
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Backend</title><head>');
     res.write('<body><h1>arughaiouerhgar</h1></body>');
     res.write('</html>');
 });
-console.log('asdfa');
-server.listen(5000);
+server.listen(3000);
 
