8aa6fbdcf3efc61190799ab14e364bce33d07b45
jsonggan <1006283@mymail.sutd.edu.sg>
2023-05-12 01:42:31 +0800

    add data to database

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
+const routes = require('./server/routes/movieRoutes.js');
+app.use('/', routes);
 
+app.listen(port, ()=> console.log(`listening on port ${port}`));
 
 
---
+++ server/routes/movieRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const movieController = require('../controllers/movieController');
+
+router.get('/api/movies/', movieController.listMovies);
+
+module.exports = router;
