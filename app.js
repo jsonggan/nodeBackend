3885a858a0f97836dd3b963121f8a29d39e4350e
chin song <1006283@mymail.sutd.edu.sg>
2023-05-11 22:52:06 +0800

    added linter & cors & middleware

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const adminData= require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-//parse all the respond so that i can print it out or just use it
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
-app.use("/admin",adminData.routes);
-app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
 
-app.listen(3000);
 
